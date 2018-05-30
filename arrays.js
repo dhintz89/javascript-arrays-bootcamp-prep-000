a27484d741a29bc761f7ed11e22d1f31bcc9aac0
Dan Hintz <dhintz@live.com>
2018-05-30 20:29:52 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -38,5 +38,6 @@ function addElementToBeginningOfArray(array,element) {
   return [element,...array];
 }
 
-
+function destructivelyAddElementToBeginningOfArray(array,element) {
+  return array.unshift(element);
 }
